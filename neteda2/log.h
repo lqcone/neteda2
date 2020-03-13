445f0c06e8441816aeebc50aba21c9e08775a6ae
chard lee <lqcone1@163.com>
2020-03-13 11:10:21 +0800

    添加源码

---
+++ neteda2/log.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdarg.h>
+
+
+
+extern unsigned long long debug_flags;
+
+
+
+#define debug(type, args...) do { if(unlikely(!silent && (debug_flags & type))) debug_int(__FILE__, __FUNCTION__, __LINE__, ##args); } while(0)
+#define info(args...)    info_int(__FILE__, __FUNCTION__, __LINE__, ##args)
+
+
+
+extern void log_date(FILE* out);
+extern void debug_int(const char* file, const char* function, const unsigned long line, const char* fmt, ...);
+extern void info_int(const char* file, const char* function, const unsigned long line, const char* fmt, ...);
---
+++ neteda2/main.c
@@ -0,0 +1,9 @@
+
+#include "log.h"
+
+void main()
+{
+    printf("hello from neteda2!\n");
+    info("hello,this is info function");
+    
+}
